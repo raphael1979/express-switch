39de67bc6e0d68a95a621c7a755ada89768c4e4c
raphael <moudens.raphael@orange.fr>
2017-03-01 11:14:05 +0100

    ajout formulaire.html, link server.js et json

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var path = require("path");
 var fs = require('fs');
+var datajson = require("data/crm.json");
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -14,9 +15,19 @@ app.get('/', function(req, res){
 	response.sendFile(path.join(__dirname + "public/index.html"));
 });
 
+app.get('/formulaire', function(req, res) {
+	response.sendFile(path.join(__dirname, 'public/formulaire.html'));
+});
+
+app.get('/data', function(req, res) {
+	response.send(datajson);
+});
+
 app.listen(1337, function(){
 	console.log('ca marche')
-})
+});
+
+
 
 
 
