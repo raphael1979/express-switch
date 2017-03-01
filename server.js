a2530e1c3100a1f4861fa487183ecc75f3a29b19
raphael <moudens.raphael@orange.fr>
2017-03-01 10:39:30 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
+var fs = require('fs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.get('/', function(req, res){
+	response.sendFile(path.join(__dirname + "public/index.html"));
+});
+
+
+app.listen(1337, function(){
+	console.log('ca marche')
+})
+
+
+
 // write you're code here Good Luck
